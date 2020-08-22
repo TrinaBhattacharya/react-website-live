4b8152aaf971518f1ed0d563f0631423f9050e4d
TrinaBhattacharya <trin9501@gmail.com>
2020-08-22 19:43:35 +0530

    Updates

---
+++ precache-manifest.310bc0f854c7a8f63becb94d44f1191f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b55f619072c74c638a44e556e2fcea",
+    "url": "/react-website-live/index.html"
+  },
+  {
+    "revision": "c8069122e07b5f4196c9",
+    "url": "/react-website-live/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "8359abe299378ce4a46a",
+    "url": "/react-website-live/static/css/main.81849ea7.chunk.css"
+  },
+  {
+    "revision": "c8069122e07b5f4196c9",
+    "url": "/react-website-live/static/js/2.7094b250.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-website-live/static/js/2.7094b250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8359abe299378ce4a46a",
+    "url": "/react-website-live/static/js/main.aae1fb30.chunk.js"
+  },
+  {
+    "revision": "8182ad32fbb555dcde37",
+    "url": "/react-website-live/static/js/runtime-main.6ca3f36c.js"
+  },
+  {
+    "revision": "8659f78aa429807662a7ec251dca8f52",
+    "url": "/react-website-live/static/media/digital.8659f78a.jpg"
+  },
+  {
+    "revision": "14c7dcf0155a5504bb5812ab1eec6032",
+    "url": "/react-website-live/static/media/img2.14c7dcf0.jpg"
+  }
+]);
